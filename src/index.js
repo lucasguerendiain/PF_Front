5b971c3ca3573b824e9902f0ffd8d02896aaeab9
DamianBroglia <dnbdt85@gmail.com>
2023-05-06 16:12:08 -0300

    DeployInNetlify

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from "./redux/store/store";
 import axios from "axios"
+axios.defaults.url = "https://pfback-production.up.railway.app"
 
 
 
