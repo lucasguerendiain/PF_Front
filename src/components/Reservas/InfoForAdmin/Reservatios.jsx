305e4a3c5a689908f38c60c21afdf02a20ab19cc
Elias Yanzon <eliasmotog3@gmail.com>
2023-05-17 17:28:24 -0300

    ahora se ven las reservas en la parte de reservas en la dashboard

--- src/components/Reservas/InfoForAdmin/Reservatios.jsx
+++ src/components/Reservas/InfoForAdmin/Reservatios.jsx
@@ -2,6 +2,9 @@ import React, { useEffect }from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { getAllreservas } from "../../../redux/actions/reservaActions";
+import LoadingComponent from "../../Loading/LoadingComponent"
+import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 export default function Reservations(){
     const reserva = useSelector((state) => state.reserva.allReservas)
@@ -13,6 +16,25 @@ export default function Reservations(){
     },[dispatch])
 
     return(
-      <div></div>  
+        <Box sx={{ padding: '16px' }}>
+        {reserva ? (
+          reserva.map((r) => (
+            <Box key={r.id} s  
+            sx={{
+                marginBottom: "16px",
+                border: "1px solid black",
+                borderRadius: "4px",
+                padding: "16px",
+              }}
+              >
+              <Typography variant="h5">Reserva</Typography>
+              <Typography variant="h6">Nombre del paquete: {r.package.name}</Typography>
+              <Typography variant="h6">Usuario: {r.user.email}</Typography>
+            </Box>
+          ))
+        ) : (
+          <LoadingComponent />
+        )}
+      </Box> 
     )
 }
