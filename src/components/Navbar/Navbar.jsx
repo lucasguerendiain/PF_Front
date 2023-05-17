b9d44071a2627b72567f64aa41c2065cffaebbf2
Lucas Guerendiain <lucasguere@hotmail.com>
2023-05-17 18:50:24 -0300

    boton admin a la navBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaUser, FaShoppingCart } from 'react-icons/fa';
-import { useDispatch} from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
 import { addUser } from "../../redux/actions/UserActions";
 import style from './Navbar.css';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -19,8 +19,9 @@ import LogoutIcon from '@mui/icons-material/Logout';
 const Navbar = () => {
   const {logout, loginWithRedirect, user, isAuthenticated, isLoading, error } = useAuth0();
   const dispatch = useDispatch();
+  const admin = process.env.REACT_APP_ADMIN_USERS;
 
-  const [data, setData] =useState({
+  const [data, setData] = useState({
     email : "",
     name: "",
     lastName:"",
@@ -78,11 +79,12 @@ const Navbar = () => {
                   Inicio
                 </Link>
               </li>
-              {/* <li className={style.navItem + ' nav-item'}>
+              {admin.slice(",").includes(user.email)
+              ? (<li className={style.navItem + ' nav-item'}>
                 <Link className={style.navLink + ' nav-link'} to='/dashboard'>
                   Administrador
                 </Link>
-              </li> */}
+            </li>) : ("")}
               <li className={style.navItem + ' nav-item'}>
                 <Link className={style.navLink + ' nav-link'} to='/packagecards'>
                   Paquetes
@@ -179,11 +181,12 @@ const Navbar = () => {
                   Inicio
                 </Link>
               </li>
-              {/* <li className={style.navItem + ' nav-item'}>
+              {/*
+                <li className={style.navItem + ' nav-item'}>
                 <Link className={style.navLink + ' nav-link'} to='/dashboard'>
                   Administrador
                 </Link>
-              </li> */}
+              </li>) */}
               <li className={style.navItem + ' nav-item'}>
                 <Link className={style.navLink + ' nav-link'} to='/packagecards'>
                   Paquetes
