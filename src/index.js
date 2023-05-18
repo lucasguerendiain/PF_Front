f03fd6d46e40510f760e6f667461b0805d680c9a
DamianBroglia <dnbdt85@gmail.com>
2023-05-18 09:56:48 -0300

    ADeploy

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 import axios from 'axios';
 
-// axios.defaults.baseURL= "https://pfback-production.up.railway.app"
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL= "https://pfback-production.up.railway.app"
+// axios.defaults.baseURL = 'http://localhost:3001';
 
 const root = document.getElementById('root');
 
